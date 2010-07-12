9346605c09ec735935e8b97dbdd49c801192d787
sones <info@sones.com>
2010-07-12 10:14:49 +0200

    small bugfix in clean up script

--- clearDirectory.sh
+++ clearDirectory.sh
@@ -7,7 +7,7 @@ for Dirs in $(ls -ad $1); do
 	if [ -d $Dirs ] ; then
 		for entry in $@; do
 			if [ $entry != $1 ] ; then
-				files=$(find . -name "$entry")
+				files=$(find $1 -name "$entry")
 				for files in $files; do
 					rm -f $files
 					echo "the following file was removed:" $files
