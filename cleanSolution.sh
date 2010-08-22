be0b414339937f08215176d03114b938a40ea0d1
Daniel Kirstenpfad <bietiekay@buero145.buero.sones.de>
2010-08-22 13:33:37 +0200

    edited run script to work from everywhere

--- cleanSolution.sh
+++ cleanSolution.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-./clearDirectory.sh Applications *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh GraphAlgorithms *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh GraphDB *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh GraphDS *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh GraphFS *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh Lib *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh Libraries *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh Notifications *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh StorageEngines *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
-./clearDirectory.sh GraphIO *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog
+./clearDirectory.sh Applications *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh GraphAlgorithms *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh GraphDB *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh GraphDS *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh GraphFS *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh Lib *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh Libraries *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt  *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh Notifications *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh StorageEngines *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
+./clearDirectory.sh GraphIO *.dll *.exe *.mdb *.pdb *.FilesWrittenAbsolute.txt *.cache *.FileListAbsolute.txt *.vspscc *.vssscc *.tlog *.pidb
 
 rm -r GraphDS/GraphDSREST/obj/Debug
--- run_sonesExample.sh
+++ run_sonesExample.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-cd ./Applications/sonesExample/bin/Debug/
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+echo Starting sonesExample in $DIRECTORY/Applications/sonesExample/bin/Debug/
+
+cd $DIRECTORY/Applications/sonesExample/bin/Debug/
 mono --runtime=v4.0.30319 sonesExample.exe
