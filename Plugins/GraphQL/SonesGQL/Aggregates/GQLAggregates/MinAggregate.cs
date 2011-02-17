a27b380fbec3d29b490acc1d15a59e8e90980e86
Henning Rauch <henning@sones.de>
2011-02-17 14:17:54 +0000

    +refactoring
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2159

---
+++ Plugins/GraphQL/SonesGQL/Aggregates/GQLAggregates/MinAggregate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using sones.Plugins.SonesGQL;
+
+namespace GQLAggregates
+{
+    public sealed class MinAggregate : IGQLAggregate
+    {
+        #region IGQLAggregate Members
+
+        public string Name
+        {
+            get { return "MIN"; }
+        }
+
+        #endregion
+    }
+}
