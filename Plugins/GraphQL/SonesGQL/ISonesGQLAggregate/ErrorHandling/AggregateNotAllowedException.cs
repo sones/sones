25f81763c88f2aead6be4c9e78a6a0bef3c822a9
Alexander Fiedler <alexander.fiedler@sones.de>
2011-03-25 10:59:43 +0000

    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2426

---
+++ Plugins/GraphQL/SonesGQL/ISonesGQLAggregate/ErrorHandling/AggregateNotAllowedException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using sones.Library.ErrorHandling;
+
+namespace sones.Plugins.SonesGQL.Aggregates.ErrorHandling
+{
+    public sealed class AggregateNotAllowedException : ASonesQLAggregateException
+    {
+        public ChainPartFuncDefinition Aggregate { get; private set; }
+
+        /// <summary>
+        /// Creates a new AggregateNotAllowedException exception
+        /// </summary>
+        /// <param name="myAggregateNode"></param>
+        public AggregateNotAllowedException(ChainPartFuncDefinition myAggregateNode)
+        {
+            Aggregate = myAggregateNode;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("The aggregate \"{0}\" is not allowed in this context!", Aggregate);
+        }
+
+        public override ushort ErrorCode
+        {
+            get { return ErrorCodes.AggregateNotAllowed; }
+        }  
+    }
+}
