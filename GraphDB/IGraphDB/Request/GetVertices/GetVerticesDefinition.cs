f07736f1806099cef471afd5ca699f81d6487045
Henning Rauch <henning@sones.de>
2011-04-01 10:45:52 +0000

    +some gluecode for the get-vertex functionality
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2481

---
+++ GraphDB/IGraphDB/Request/GetVertices/GetVerticesDefinition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using sones.GraphDB.Request.Helper.Expression;
+
+namespace sones.GraphDB.Request
+{
+    /// <summary>
+    /// The definition of what kind of vertices should be requested from the graphdb
+    /// </summary>
+    public sealed class GetVerticesDefinition
+    {
+        #region Data
+
+        /// <summary>
+        /// The expression which should be evaluated
+        /// </summary>
+        public readonly IExpression Expression;
+
+        /// <summary>
+        /// Determines whether it is anticipated that the request could take longer
+        /// </summary>
+        public readonly Boolean IsLongrunning;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new get vertices definition
+        /// </summary>
+        /// <param name="myExpression">The expression which should be evaluated</param>
+        /// <param name="myIsLongrunning">Determines whether it is anticipated that the request could take longer</param>
+        public GetVerticesDefinition(IExpression myExpression, Boolean myIsLongrunning = false)
+        {
+            Expression = myExpression;
+            IsLongrunning = myIsLongrunning;
+        }
+
+        #endregion
+    }
+}
--- GraphDB/IGraphDB/Request/GetVertices/RequestGetVertices.cs
+++ GraphDB/IGraphDB/Request/GetVertices/RequestGetVertices.cs
@@ -5,10 +5,32 @@ using System.Text;
 
 namespace sones.GraphDB.Request
 {
+    /// <summary>
+    /// The get vertices request
+    /// </summary>
     public sealed class RequestGetVertices : IRequest
     {
+        #region data
 
+        /// <summary>
+        /// The definition of the vertices that should be requests from the graphdb
+        /// </summary>
+        public readonly GetVerticesDefinition GetVerticesDefinition;
 
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new request that clears the Graphdb
+        /// </summary>
+        /// <param name="myGetVerticesDefinition">The definition of the vertices that should be requests from the graphdb</param>
+        public RequestGetVertices(GetVerticesDefinition myGetVerticesDefinition)
+        {
+            GetVerticesDefinition = myGetVerticesDefinition;
+        }
+
+        #endregion
 
         #region IRequest Members
 
