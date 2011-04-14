b30ab26ec5bca80adcae0b5593db1c07561ff3cf
Dirk Bludau <dirk@sones.de>
2011-04-14 11:58:33 +0000

    added xml io query result
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2558

---
+++ Plugins/GraphDS/IO/Interface/IOInterface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mime;
+using sones.GraphQL.Result;
+using sones.Library.VersionedPluginManager;
+
+namespace sones.Plugins.GraphDS.IOInterface
+{
+    /// <summary>
+    /// This is the interface for all IO methods, which can be used in GraphDS to generate an output result.
+    /// </summary>
+    public interface IOInterface : IPluginable
+    {
+        #region Content Type
+
+        /// <summary>
+        /// Returns the content type of the special format.
+        /// </summary>
+        ContentType ContentType { get; }
+
+        #endregion
+
+        #region Output Result
+
+        /// <summary>
+        /// Generates the representation of an query result in a special format.
+        /// </summary>
+        /// <param name="myQueryResult">The result of an query.</param>
+        /// <returns>The representation of the result as string.</returns>
+        String GenerateOutputResult(QueryResult myQueryResult);
+
+        #endregion
+        
+        #region Query Result
+        
+        /// <summary>
+        /// Generates an query result from a special respresentation.
+        /// </summary>
+        /// <param name="myResult">The query result as string in a special format.</param>
+        /// <returns>An query result.</returns>
+        QueryResult GenerateQueryResult(String myResult);
+
+        #endregion
+    }
+}
