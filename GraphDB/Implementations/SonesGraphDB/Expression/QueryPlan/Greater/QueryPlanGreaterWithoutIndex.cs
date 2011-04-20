b0143a026d7fc850aef9969c555248cf175807c2
Henning Rauch <henning@sones.de>
2011-04-20 11:15:46 +0000

    +added Lower and Greater operators
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2620

---
+++ GraphDB/Implementations/SonesGraphDB/Expression/QueryPlan/Greater/QueryPlanGreaterWithoutIndex.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using sones.Library.PropertyHyperGraph;
+using System;
+using sones.Library.Commons.VertexStore;
+using sones.GraphDB.TypeSystem;
+using sones.Library.Commons.Security;
+using sones.Library.Commons.Transaction;
+
+namespace sones.GraphDB.Expression.QueryPlan
+{
+    /// <summary>
+    /// An Greater operation without any index
+    /// </summary>
+    public sealed class QueryPlanGreaterWithoutIndex : AComparativeOperator, IQueryPlan
+    {
+        #region constructor
+
+        /// <summary>
+        /// Creates a new queryplan that processes an greater operation without any index
+        /// </summary>
+        /// <param name="mySecurityToken">The current security token</param>
+        /// <param name="myTransactionToken">The current transaction token</param>
+        /// <param name="myProperty">The interesting property</param>
+        /// <param name="myConstant">The constant value</param>
+        /// <param name="myVertexStore">The vertex store that is needed to load the vertices</param>
+        /// <param name="myIsLongrunning">Determines whether it is anticipated that the request could take longer</param>
+        public QueryPlanGreaterWithoutIndex(SecurityToken mySecurityToken, TransactionToken myTransactionToken, QueryPlanProperty myProperty, QueryPlanConstant myConstant, IVertexStore myVertexStore, Boolean myIsLongrunning)
+            : base(myProperty, myConstant, myIsLongrunning, mySecurityToken, myTransactionToken, myVertexStore)            
+        {
+        }
+
+        #endregion
+
+        #region IQueryPlan Members
+
+        public IEnumerable<IVertex> Execute()
+        {
+            return Execute_protected(_property.VertexType);
+        }
+
+        #endregion
+
+        #region overrides
+
+        public override IEnumerable<IVertex> GetMatchingVertices(IVertexType myInterestingVertexType)
+        {
+            foreach (var aVertex in _vertexStore.GetVerticesByTypeID(_securityToken, _transactionToken, myInterestingVertexType.ID, _property.Edition, VertexRevisionFilter))
+            {
+                if (aVertex.HasProperty(_property.Property.AttributeID))
+                {
+                    if (_property.Property.ExtractValue(aVertex).CompareTo(_constant.Constant) > 0)
+                    {
+                        yield return aVertex;
+                    }
+                }
+            }
+
+            yield break;
+        }
+
+        #endregion
+    }
+}
---
+++ GraphDB/Implementations/SonesGraphDB/Expression/QueryPlan/Lower/QueryPlanLowerWithIndex.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using sones.Library.PropertyHyperGraph;
+using System;
+using sones.Library.Commons.VertexStore;
+using sones.GraphDB.TypeSystem;
+using sones.GraphDB.Manager.Index;
+using sones.Library.Commons.Security;
+using sones.Library.Commons.Transaction;
+using sones.Plugins.Index.Interfaces;
+using System.Linq;
+
+namespace sones.GraphDB.Expression.QueryPlan
+{
+    /// <summary>
+    /// An Lower operation using indices
+    /// </summary>
+    public sealed class QueryPlanLowerWithIndex : AComparativeIndexOperator, IQueryPlan
+    {
+        #region constructor
+
+        /// <summary>
+        /// Creates a new queryplan that processes a Lower operation using indices
+        /// </summary>
+        /// <param name="mySecurityToken">The current security token</param>
+        /// <param name="myTransactionToken">The current transaction token</param>
+        /// <param name="myProperty">The interesting property</param>
+        /// <param name="myConstant">The constant value</param>
+        /// <param name="myVertexStore">The vertex store that is needed to load the vertices</param>
+        /// <param name="myIsLongrunning">Determines whether it is anticipated that the request could take longer</param>
+        public QueryPlanLowerWithIndex(SecurityToken mySecurityToken, TransactionToken myTransactionToken, QueryPlanProperty myProperty, QueryPlanConstant myConstant, IVertexStore myVertexStore, Boolean myIsLongrunning, IIndexManager myIndexManager)
+            : base(myProperty, myConstant, myIsLongrunning, mySecurityToken, myTransactionToken, myIndexManager, myVertexStore)        
+        {
+        }
+
+        #endregion
+
+        #region IQueryPlan Members
+        
+        public IEnumerable<IVertex> Execute()
+        {
+            return Execute_protected(_property.VertexType);
+        }
+
+        #endregion
+
+        #region overrides
+
+        public override IIndex<IComparable, long> GetBestMatchingIdx(IEnumerable<IIndex<IComparable, long>> myIndexCollection)
+        {
+            return myIndexCollection.First();
+        }
+
+        public override IEnumerable<long> GetSingleIndexValues(ISingleValueIndex<IComparable, long> mySingleValueIndex, IComparable myIComparable)
+        {
+            if (mySingleValueIndex is IRangeIndex<IComparable, long>)
+            {
+                //use the range funtionality
+
+                foreach (var aVertexID in ((ISingleValueRangeIndex<IComparable, long>)mySingleValueIndex).LowerThan(myIComparable, false))
+                {
+                    yield return aVertexID;
+                }
+            }
+            else
+            {
+                //stupid, but works
+
+                foreach (var aVertexID in mySingleValueIndex.Where(kv => kv.Key.CompareTo(myIComparable) < 0).Select(kv => kv.Value))
+                {
+                    yield return aVertexID;
+                }
+
+            }
+
+            yield break;
+        }
+
+        public override IEnumerable<long> GetMultipleIndexValues(IMultipleValueIndex<IComparable, long> myMultipleValueIndex, IComparable myIComparable)
+        {
+            if (myMultipleValueIndex is IRangeIndex<IComparable, long>)
+            {
+                //use the range funtionality
+
+                foreach (var aVertexIDSet in ((IMultipleValueRangeIndex<IComparable, long>)myMultipleValueIndex).LowerThan(myIComparable, false))
+                {
+                    foreach (var aVertexID in aVertexIDSet)
+                    {
+                        yield return aVertexID;
+                    }
+                }
+            }
+            else
+            {
+                //stupid, but works
+
+                foreach (var aVertexIDSet in myMultipleValueIndex.Where(kv => kv.Key.CompareTo(myIComparable) < 0).Select(kv => kv.Value))
+                {
+                    foreach (var aVertexID in aVertexIDSet)
+                    {
+                        yield return aVertexID;
+                    }
+                }
+            }
+
+            yield break;
+        }
+
+        #endregion
+    }
+}
