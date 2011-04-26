6c92eb74036059f5e0dcabdde2537c6df9121321
Michael Woidak <michael@sones.de>
2011-04-26 09:03:36 +0000

    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2651

---
+++ Plugins/GraphQL/SonesGQL/Aggregates/GQLAggregates/MaxAggregate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ISonesGQLFunction.Structure;
+using sones.Plugins.Index.Interfaces;
+using sones.GraphDB.TypeSystem;
+using sones.GraphDB;
+using sones.Library.Commons.Security;
+using sones.Library.Commons.Transaction;
+using sones.Library.PropertyHyperGraph;
+using sones.Library.VersionedPluginManager;
+
+namespace sones.Plugins.SonesGQL.Aggregates
+{
+    public sealed class MaxAggregate : IGQLAggregate
+    {
+        #region constructor
+
+        /// <summary>
+        /// Creates a new max aggregate
+        /// </summary>
+        public MaxAggregate()
+        {
+
+        }
+
+        #endregion
+
+        #region IGQLAggregate Members
+
+        public FuncParameter Aggregate(IIndex<IComparable, long> myAttributeIndex, IVertexType myGraphDBType, IGraphDB myGraphDB, SecurityToken mySecurityToken, TransactionToken myTransactionToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FuncParameter Aggregate(IEnumerable<IVertex> myDBObjects, IAttributeDefinition myTypeAttribute, IGraphDB myGraphDB, SecurityToken mySecurityToken, TransactionToken myTransactionToken, params ParameterValue[] myParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IPluginable Members
+
+        public string PluginName
+        {
+            get { return "MAX"; }
+        }
+
+        public Dictionary<string, Type> SetableParameters
+        {
+            get { return new Dictionary<string, Type>(); }
+        }
+
+        public IPluginable InitializePlugin(Dictionary<string, object> myParameters = null)
+        {
+            return new MaxAggregate();
+        }
+
+        #endregion
+    }
+}
--- Plugins/GraphQL/SonesGQL/Aggregates/GQLAggregates/MinAggregate.cs
+++ Plugins/GraphQL/SonesGQL/Aggregates/GQLAggregates/MinAggregate.cs
@@ -8,21 +8,72 @@ using sones.Library.PropertyHyperGraph;
 using sones.Library.Commons.Transaction;
 using sones.Library.Commons.Security;
 using sones.Library.VersionedPluginManager;
+using sones.GraphDB.Request;
+using System.Linq;
 
 namespace sones.Plugins.SonesGQL.Aggregates
 {
     public sealed class MinAggregate : IGQLAggregate
     {
+        #region constructor
+
+        /// <summary>
+        /// Creates a new min aggregate
+        /// </summary>
+        public MinAggregate()
+        {
+
+        }
+
+        #endregion
+
         #region IGQLAggregate Members
 
         public FuncParameter Aggregate(IIndex<IComparable, long> myAttributeIndex, IVertexType myGraphDBType, IGraphDB myGraphDB, SecurityToken mySecurityToken, TransactionToken myTransactionToken)
         {
-            throw new NotImplementedException();
+            return new FuncParameter(myAttributeIndex.Keys().Min());
         }
 
         public FuncParameter Aggregate(IEnumerable<IVertex> myDBObjects, IAttributeDefinition myTypeAttribute, IGraphDB myGraphDB, SecurityToken mySecurityToken, TransactionToken myTransactionToken, params ParameterValue[] myParameters)
         {
-            throw new NotImplementedException();
+            IComparable aggregateResult = null;
+
+            var foundFirstMin = false;
+
+            if (myTypeAttribute is IPropertyDefinition)
+            {
+                foreach (var dbo in myDBObjects)
+                {
+                    if (dbo.HasProperty(myTypeAttribute.AttributeID))
+                    {
+                        var attrResult = dbo.GetProperty(myTypeAttribute.AttributeID);
+
+                        if (foundFirstMin == false)
+                        {
+                            aggregateResult = attrResult;
+
+                            foundFirstMin = true;
+                        }
+                        else
+                        {
+                            #region Compare current with min value
+
+                            if (aggregateResult.CompareTo(attrResult) > 0)
+                            {
+                                aggregateResult = attrResult;
+                            }
+
+                            #endregion
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return new FuncParameter(new AggregateException("Aggregate not valid on type " + myTypeAttribute.Name));
+            }
+
+            return new FuncParameter(aggregateResult);
         }
 
         #endregion
@@ -31,17 +82,17 @@ namespace sones.Plugins.SonesGQL.Aggregates
 
         public string PluginName
         {
-            get { throw new NotImplementedException(); }
+            get { return "MIN"; }
         }
 
         public Dictionary<string, Type> SetableParameters
         {
-            get { throw new NotImplementedException(); }
+            get { return new Dictionary<string, Type>(); }
         }
 
         public IPluginable InitializePlugin(Dictionary<string, object> myParameters = null)
         {
-            throw new NotImplementedException();
+            return new MinAggregate();
         }
 
         #endregion
