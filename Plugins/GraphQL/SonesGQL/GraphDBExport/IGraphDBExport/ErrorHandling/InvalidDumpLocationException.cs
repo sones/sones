3de1e37e05fdc0c0de6e99207ca4818c62e6ad80
Michael Woidak <michael@sones.de>
2011-05-03 07:48:08 +0000

    adding exporter as new project
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2907

---
+++ Plugins/GraphQL/SonesGQL/GraphDBExport/IGraphDBExport/ErrorHandling/InvalidDumpLocationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sones.Plugins.SonesGQL.DBExport
+{
+    public class InvalidDumpLocationException : ASonesQLGraphDBExportException
+    {
+        private String Location;
+        private String Info;
+        private String ExpectedFile;
+        private String ExpectedHttp;
+
+        public InvalidDumpLocationException(String myLocation, String myExpectedFile, String myExpectedHttp, String myInfo)
+        {
+            Location = myLocation;
+            Info = myInfo;
+            ExpectedFile = myExpectedFile;
+            ExpectedHttp = myExpectedHttp;
+
+            _msg = String.Format("Invalid dump destination: {0}. Expected {1} or {2}\n\n{3}", Location, myExpectedFile, myExpectedHttp, Info);
+        }
+
+    }
+}
---
+++ Plugins/GraphQL/SonesGQL/GraphDBExport/IGraphDBExport/ErrorHandling/StreamReaderException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sones.Plugins.SonesGQL.DBExport
+{
+    public class StreamReaderException : ASonesQLGraphDBExportException
+    {
+        private String Stream;
+        private String Info;
+        private Exception InnerException;
+
+        public StreamReaderException(String myStream, String myInfo, Exception myInnerException)
+        {
+            Stream = myStream;
+            Info = myInfo;
+            InnerException = myInnerException;
+
+            if(InnerException != null)
+                _msg = String.Format("Error during initializing new {0} StreamReader.\n\n{1}\n\n{2}", Stream, InnerException.Message, Info);
+            else
+                _msg = String.Format("Error during initializing new {0} StreamReader.\n\n{1}", Stream, Info);
+        }
+    }
+}
