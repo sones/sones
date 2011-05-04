b4213171dffdf229f180ecd0c70815cbac39657f
David Maier <david@sones.de>
2011-05-04 11:56:09 +0000

    Added Type extension
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C2957

---
+++ Library/Internal/LanguageExtensions/TypeExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sones.LanguageExtensions
+{
+    public static class TypeExtensions
+    {
+
+        /// <summary>
+        /// Determines if the type implements a specific interface type
+        /// </summary>
+        /// <param name="myType"></param>
+        /// <param name="myInterfaceType"></param>
+        /// <returns></returns>
+        public static bool HasInterface(this Type myType, Type myInterfaceType)
+        {
+
+            Type[] array = myType.FindInterfaces(
+                delegate(Type typeObj, Object criteriaObj)
+                {
+
+                    return typeObj.Equals((Type)criteriaObj);
+
+                },
+
+                myInterfaceType
+
+            );
+
+            return array.Length > 0;
+
+        }
+    }
+}
