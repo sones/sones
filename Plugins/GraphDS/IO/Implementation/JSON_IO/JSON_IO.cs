5f9f013289c637202ac9013105b1da30ef040885
Dirk Bludau <dirk@sones.de>
2011-05-10 19:17:59 +0000

    changes on json output
    git-tfs-id: [http://tfs2010.sones:8080/tfs/sones]$/Experimental/IGraphFSReDesign;C3170

--- Plugins/GraphDS/IO/Implementation/JSON_IO/JSON_IO.cs
+++ Plugins/GraphDS/IO/Implementation/JSON_IO/JSON_IO.cs
@@ -215,26 +215,7 @@ namespace sones.Plugins.GraphDS.IO.JSON_IO
             }
             else
             {
-                //var edgeProperties = new JArray();
-                //JProperty _newEdge = null;
-
-                //foreach (var _property in aEdge.GetAllProperties())
-                //{
-                //    if (_property.Item2 == null)
-                //        _newEdge = new JProperty(_property.Item1, "");
-                //    else
-                //        if (_property.Item2 is Stream)
-                //        {
-                //            _newEdge = new JProperty(_property.Item1, "BinaryProperty");
-                //        }
-                //        else
-                //            _newEdge = new JProperty(_property.Item1, _property.Item2.ToString());
-
-                //    edgeProperties.Add(new JObject(_newEdge));
-                //}
-
                 Output.Add(new JObject(new JProperty("TargetVertex", GenerateVertexViewJSON(((ISingleEdgeView)aEdge).GetTargetVertex()))));
-
             }
 
             return Output;
