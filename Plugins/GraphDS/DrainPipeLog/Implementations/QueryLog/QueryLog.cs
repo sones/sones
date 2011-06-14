06fa811fce4809a900029d28680a44b62170d5d5
Timo <timo.wagner@sones.com>
2011-06-14 16:19:01 +0200

    Added a simple query log based on IDrainPipe.

--- Plugins/GraphDS/DrainPipeLog/Implementations/QueryLog/QueryLog.cs
+++ Plugins/GraphDS/DrainPipeLog/Implementations/QueryLog/QueryLog.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using sones.GraphDB;
 using sones.GraphDB.Request;
 using sones.GraphDB.Request.GetEdgeType;
