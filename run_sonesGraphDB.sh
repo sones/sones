8bdee8f60994236095cdab02864b09252d535979
Daniel Kirstenpfad <daniel.kirstenpfad@sones.com>
2011-06-18 15:02:12 +0200

    - removed unnecessary Debug_x64/x86 and Release_x64/x86 from solution and project files

--- run_sonesGraphDB.sh
+++ run_sonesGraphDB.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 option="-d"
+
 if [ $# -ne 1 ]; then
-echo "Type -h for build options. Defaulting to debug."
+	echo "Type -h for build options. Defaulting to debug."
 elif [ $1 = "-r" ]; then
-option=$1
+	option=$1
 fi
 
 
 if [ $1 = "-h" ]; then
-echo "-r run a release"
-echo "-d run with debug symbols"
-exit 0
+	echo "-r run a release"
+	echo "-d run with debug symbols"
+	exit 0
 elif [ $1 != "-d" -a $1 != "-r" ]; then
-exit 0
+	exit 0
 fi
 
 DIRECTORY=$(cd `dirname $0` && pwd)
