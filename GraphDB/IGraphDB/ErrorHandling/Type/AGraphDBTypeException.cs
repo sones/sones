7f5273c92955c14dd147ea9b87c0acf8e6de7128
Timo <timo.wagner@sones.com>
2011-06-29 09:57:07 +0200

    Fixed namespace in errorhandling.

--- GraphDB/IGraphDB/ErrorHandling/Type/AGraphDBTypeException.cs
+++ GraphDB/IGraphDB/ErrorHandling/Type/AGraphDBTypeException.cs
@@ -23,7 +23,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace sones.GraphDB.ErrorHandling.Type
+namespace sones.GraphDB.ErrorHandling
 {
     public abstract class AGraphDBTypeException: AGraphDBException
     {
