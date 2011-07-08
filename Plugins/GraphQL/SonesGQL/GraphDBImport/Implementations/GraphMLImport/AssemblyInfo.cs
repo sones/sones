d0922b5f8d3c32978e81a68cccfdb1d451e306fa
Daniel Kirstenpfad <daniel.kirstenpfad@sones.com>
2011-07-08 14:19:16 +0200

    added license header

--- Plugins/GraphQL/SonesGQL/GraphDBImport/Implementations/GraphMLImport/AssemblyInfo.cs
+++ Plugins/GraphQL/SonesGQL/GraphDBImport/Implementations/GraphMLImport/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("GraphMLImport")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("s1ck")]
+[assembly: AssemblyCompany("sones GmbH")]
+[assembly: AssemblyProduct("GraphMLImport")]
+[assembly: AssemblyCopyright("Copyright © sones GmbH 2007-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
