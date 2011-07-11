6d31d8848ce649be02c1261fc559a229cb1b7aa3
Michael Woidak <m.woidak@gmx.de>
2011-07-11 11:58:08 -0700

    added some documentation to the system informations

--- Library/Internal/SystemInformation/Intern/MonoSystemInformation.cs
+++ Library/Internal/SystemInformation/Intern/MonoSystemInformation.cs
@@ -8,27 +8,40 @@ using System.Diagnostics;
 
 namespace sones.Library.SystemInformation.Intern
 {
-    internal sealed class MonoSystemInformation: SystemInformation
+    /// <summary>
+    /// This class provides specific plattform informations like total and available main memory.
+    /// </summary>
+    internal sealed class MonoSystemInformation : SystemInformation
     {
-
-
+        /// <summary>
+        /// Reads out the free space for the given path.
+        /// </summary>
         public override ulong GetFreeSpaceForPath(string myPath)
         {
             return ulong.MaxValue;
         }
 
+        /// <summary>
+        /// Reads out the available main memory.
+        /// </summary>
         public override ulong GetAvailableMainMemory()
         {
             //an estimation
             return GetTotalMainMemory() - GetMainMemoryConsumption();
         }
 
+        /// <summary>
+        /// Reads out the total main memory.
+        /// </summary>
         public override ulong GetTotalMainMemory()
         {
             var pc = new PerformanceCounter("Mono Memory", "Total Physical Memory");
             return (ulong)pc.RawValue;
         }
 
+        /// <summary>
+        /// Reads out the consumed main memory.
+        /// </summary>
         public override ulong GetMainMemoryConsumption()
         {
             Process.GetCurrentProcess().Refresh();
--- Library/Internal/SystemInformation/Intern/WindowsSystemInformation.cs
+++ Library/Internal/SystemInformation/Intern/WindowsSystemInformation.cs
@@ -1,38 +1,52 @@
 ﻿#if !__MonoCS__
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Diagnostics;
 using Microsoft.VisualBasic.Devices;
 
 namespace sones.Library.SystemInformation.Intern
 {
-    internal sealed class WindowsSystemInformation: SystemInformation
+    /// <summary>
+    /// This class provides specific plattform informations like total and available main memory.
+    /// </summary>
+    internal sealed class WindowsSystemInformation : SystemInformation
     {
         private ComputerInfo _info;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         internal protected WindowsSystemInformation()
         {
             _info = new ComputerInfo();
         }
 
+        /// <summary>
+        /// Reads out the free space for the given path.
+        /// </summary>
         public override ulong GetFreeSpaceForPath(string myPath)
         {
             return ulong.MaxValue;
         }
 
+        /// <summary>
+        /// Reads out the available main memory.
+        /// </summary>
         public override ulong GetAvailableMainMemory()
         {
             return _info.AvailablePhysicalMemory;
         }
 
+        /// <summary>
+        /// Reads out the total main memory.
+        /// </summary>
         public override ulong GetTotalMainMemory()
         {
             return _info.TotalPhysicalMemory;
         }
 
+        /// <summary>
+        /// Reads out the consumed main memory.
+        /// </summary>
         public override ulong GetMainMemoryConsumption()
         {
             Process.GetCurrentProcess().Refresh();
