54f76ce87dfccf848ec0557e96e33577d62002be
Michael Schilonka <michael.schilonka@sones.com>
2011-08-10 14:20:57 +0200

    added RemoteAPIService

---
+++ Plugins/GraphDS/Services/Implementations/RemoteAPIService/RemoteAPIService.cs
@@ -0,0 +1,105 @@
+﻿/*
+* sones GraphDB - Community Edition - http://www.sones.com
+* Copyright (C) 2007-2011 sones GmbH
+*
+* This file is part of sones GraphDB Community Edition.
+*
+* sones GraphDB is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, version 3 of the License.
+* 
+* sones GraphDB is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with sones GraphDB. If not, see <http://www.gnu.org/licenses/>.
+* 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using sones.Plugins.GraphDS.Services;
+using sones.Library.VersionedPluginManager;
+using sones.GraphDS.Services.RemoteAPIService.ServiceStatus;
+using System.Diagnostics;
+using System.Net;
+
+namespace sones.GraphDS.Services.RemoteAPIService
+{
+    public class RemoteAPIService : IService, IPluginable
+    {
+
+
+
+        #region Data
+
+        private Boolean _IsSecure;
+
+        private IGraphDS _GraphDS;
+        
+        private Stopwatch _RunningTime;
+
+        #endregion
+
+        public RemoteAPIService(){}
+
+        public RemoteAPIService(IGraphDS myGraphDS)
+        {
+        
+        }
+
+
+        public string PluginName
+        {
+            get { return "sones.RemoteAPIService"; }
+        }
+
+        public void Start(IDictionary<string, object> myStartParameter = null)
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        public AServiceStatus GetCurrentStatus()
+        {
+            return null;
+        }
+
+
+        public PluginParameters<Type> SetableParameters
+        {
+            get
+            {
+                PluginParameters<Type> Parameters = new PluginParameters<Type>();
+                Parameters.Add("GraphDS", typeof(IGraphDS));
+                Parameters.Add("IsSecure", typeof(Boolean));
+                Parameters.Add("IPAddress", typeof(IPAddress));
+                Parameters.Add("Port", typeof(ushort));
+                Parameters.Add("URI", typeof(String));
+                return Parameters;
+            }
+        }
+
+        public IPluginable InitializePlugin(string UniqueString, Dictionary<string, object> myParameters = null)
+        {
+            IGraphDS GraphDS = null;
+            if (myParameters != null && myParameters.ContainsKey("GraphDS"))
+                GraphDS = (IGraphDS)myParameters["GraphDS"];
+
+            return new RemoteAPIService(GraphDS);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Plugins/GraphDS/Services/Implementations/RemoteAPIService/ServiceStatus/RemoteAPIServiceStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using sones.Plugins.GraphDS.Services;
+using System.Net;
+
+namespace sones.GraphDS.Services.RemoteAPIService.ServiceStatus
+{
+    public class RemoteAPIServiceStatus : AServiceStatus
+    {
+        public RemoteAPIServiceStatus(IPAddress myIPAddress, ushort myPort,Boolean isSecure, Boolean isRunning, TimeSpan myRunningTime)
+        {
+            this.IPAddress = myIPAddress;
+            this.Port = myPort;
+            this.IsRunning = isRunning;
+            this.IsNetService = true;
+            this.RunningTime = myRunningTime;
+            this.OtherStatistically = new Dictionary<string, object>();
+            
+
+            String Description = "This Service starts a SOAP - based 'Remote API Server'." + Environment.NewLine + 
+                "      * You can get the WSDL file at " + (isSecure == true ? "https" : "http") + ":" + myIPAddress.Address.ToString() + ":" + myPort.ToString() + 
+                "/rpc?wsdl" + Environment.NewLine +
+                "      * You can also use the already built client wrapper with a better handling.";
+
+            OtherStatistically.Add("Description", Description);
+            OtherStatistically.Add("IsSecure", isSecure);
+        }
+
+        
+    }
+}
