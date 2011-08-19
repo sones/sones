c8aed0585b273baed9640a933fc49467bcb5deb9
Philipp Schleizer <philipp.schleizer@sones.com>
2011-08-19 10:28:16 +0200

    added new graphvis IO plugin (not implemented skeleton yet)

---
+++ Plugins/GraphDS/IO/Implementation/GRAPHVIS_IO/GRAPHVIS_IO.cs
@@ -0,0 +1,117 @@
+﻿/*
+* sones GraphDB - Community Edition - http://www.sones.com
+* Copyright (C) 2007-2011 sones GmbH
+*
+* This file is part of sones GraphDB Community Edition.
+*
+* sones GraphDB is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, version 3 of the License.
+* 
+* sones GraphDB is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with sones GraphDB. If not, see <http://www.gnu.org/licenses/>.
+* 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.IO;
+using System.Xml.Schema;
+using sones.GraphQL.Result;
+using sones.Library.Settings;
+using sones.Library.VersionedPluginManager;
+using System.Xml;
+using System.Reflection;
+using System.Text;
+
+namespace sones.Plugins.GraphDS.IO
+{
+    public class GRAPHVIS_IO : IOInterface
+    {
+
+        #region Data
+
+        /// <summary>
+        /// The io content type.
+        /// </summary>
+        private readonly ContentType _contentType;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor for a graphvis io instance.
+        /// </summary>
+        public GRAPHVIS_IO()
+        {
+            _contentType = new ContentType("application/x-sones-graphvis") { CharSet = "UTF-8" };
+        }
+
+        #endregion
+
+
+        #region IPluginable
+
+        public string PluginName
+        {
+            get { return "sones.graphvis_io"; }
+        }
+
+        public string PluginShortName
+        {
+            get { return "graphvis"; }
+        }
+
+        public PluginParameters<Type> SetableParameters
+        {
+            get { return new PluginParameters<Type>(); }
+        }
+
+        public IPluginable InitializePlugin(String myUniqueString, Dictionary<string, object> myParameters = null)
+        {
+            var result = new GRAPHVIS_IO();
+
+            return (IPluginable)result;
+        }
+
+        public void Dispose()
+        { }
+
+        #endregion
+
+        #region IOInterface
+
+        public string GenerateOutputResult(QueryResult myQueryResult, Dictionary<String, String> myParams)
+        {
+            StringBuilder Output = new StringBuilder();
+
+            return Output.ToString();
+        }
+
+        public String ListAvailParams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryResult GenerateQueryResult(string myResult)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContentType ContentType
+        {
+            get { return _contentType; }
+        }
+
+        #endregion
+
+    }
+}
