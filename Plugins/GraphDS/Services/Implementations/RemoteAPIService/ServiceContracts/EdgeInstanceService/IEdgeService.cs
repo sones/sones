bc0190ae1633005aa85a03b19710a009b5dee40f
Michael Schilonka <michael.schilonka@sones.com>
2011-09-01 16:25:25 +0200

    added IEdgeService interface

---
+++ Plugins/GraphDS/Services/Implementations/RemoteAPIService/ServiceContracts/EdgeInstanceService/IEdgeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace sones.GraphDS.Services.RemoteAPIService.ServiceContracts.EdgeInstanceService
+{
+    [ServiceContract(Namespace = sonesRPCServer.Namespace)]
+    public interface IEdgeService : IGraphElementService
+    {
+
+    }
+}
