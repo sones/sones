2335017948d49b075c2ac2273bce440400ddaa95
Timo <timo.wagner@sones.com>
2011-09-12 15:51:57 +0200

    Now the benchmark tool closes the sonesGraphDBStarter instance it opened before.

--- Applications/SimpleImportBenchmark/Program.cs
+++ Applications/SimpleImportBenchmark/Program.cs
@@ -47,88 +47,97 @@ namespace SimpleImportBenchmark
 
                 Console.WriteLine("===  Startup ===");
                 Console.WriteLine("Starting: " + sonesGraphDBStarter);
-                Process.Start(new ProcessStartInfo(sonesGraphDBStarter) { UseShellExecute = false });
-
-                //Wait a moment to give the process a chance to get started -- So wait 10s
-                for (int i = 0; i < 100; i++)
+                var process = Process.Start(new ProcessStartInfo(sonesGraphDBStarter) { UseShellExecute = false });
+                try
                 {
-                    Thread.Sleep(100);
+
+                    //Wait a moment to give the process a chance to get started -- So wait 10s
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Thread.Sleep(100);
+                    }
+
+                    Console.WriteLine();
+
+                    //Read the configuration to determine the port, user name and password
+                    string sonesGraphDBStarterConfig = sonesGraphDBStarter + ".config";
+
+                    Dictionary<string, string> config = new Dictionary<string, string>();
+                    config.Add("userName", ReadConfig(sonesGraphDBStarterConfig, "Username"));
+                    config.Add("password", ReadConfig(sonesGraphDBStarterConfig, "Password"));
+                    config.Add("port", ReadConfig(sonesGraphDBStarterConfig, "ListeningPort"));
+
+
+                    Console.WriteLine("=== Configuration === \n" +
+                                      "User: " + config["userName"] + "\n" +
+                                      "Password: " + config["password"] + "\n" +
+                                      "Port:" + config["port"]);
+
+
+
+                    Console.WriteLine("=== Import ===");
+
+                    //Connect to the local sones GraphDB instance to start the import
+                    GraphDS_RESTClient client = new GraphDS_RESTClient("localhost", config["userName"], config["password"], UInt32.Parse(config["port"]));
+
+                    TransactionToken tx = null;
+                    SecurityToken sec = null;
+
+
+                    #region Import schema
+
+                    Console.WriteLine("Importing Schema ... ");
+
+                    var schemaQuery = "IMPORT FROM 'file:\\\\" + schemaFilePath + "' FORMAT gql";
+
+                    Console.WriteLine("Sending query: {0}", schemaQuery);
+
+                    var schema_result = client.Query(sec, tx, schemaQuery, "sones.gql");
+
+                    Console.WriteLine("... {0}", schema_result.TypeOfResult);
+
+                    if (schema_result.TypeOfResult != ResultType.Successful && schema_result.Error != null)
+                        Console.WriteLine(schema_result.Error.Message);
+
+                    #endregion
+
+
+
+                    string importQuery = null;
+
+                    if (isVerbose.Equals("True", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        importQuery = "IMPORT FROM 'file:\\\\" + dataFilePath + "' FORMAT fastimport VERBOSITY full";
+                    }
+                    else
+                    {
+                        importQuery = "IMPORT FROM 'file:\\\\" + dataFilePath + "' FORMAT fastimport";
+                    }
+
+                    Stopwatch sw = new Stopwatch();
+
+                    Console.WriteLine("Sending query: {0}", importQuery);
+
+                    Console.WriteLine("Start: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString());
+                    sw.Start();
+
+                    QueryResult rs = client.Query(sec, tx, importQuery, "sones.gql");
+
+                    sw.Stop();
+
+                    if (rs.Error != null)
+                        Console.WriteLine(rs.Error.Message);
+
+                    Console.WriteLine("Stop: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString());
+                    Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + " ms");
                 }
-
-                Console.WriteLine();
-
-                //Read the configuration to determine the port, user name and password
-                string sonesGraphDBStarterConfig = sonesGraphDBStarter + ".config";
-
-                Dictionary<string, string> config = new Dictionary<string,string>();
-                config.Add("userName",ReadConfig(sonesGraphDBStarterConfig, "Username"));
-                config.Add("password",ReadConfig(sonesGraphDBStarterConfig, "Password"));
-                config.Add("port",ReadConfig(sonesGraphDBStarterConfig, "ListeningPort"));
-
-
-                Console.WriteLine("=== Configuration === \n" +
-                                  "User: " + config["userName"] + "\n" +
-                                  "Password: " + config["password"] + "\n" +
-                                  "Port:" + config["port"]); 
-
-
-
-                Console.WriteLine("=== Import ===");
-
-                //Connect to the local sones GraphDB instance to start the import
-                GraphDS_RESTClient client = new GraphDS_RESTClient("localhost", config["userName"], config["password"], UInt32.Parse(config["port"]));
-                
-                TransactionToken tx = null;
-                SecurityToken sec = null;
-
-
-                #region Import schema
-
-                Console.WriteLine("Importing Schema ... ");
-
-                var schemaQuery = "IMPORT FROM 'file:\\\\" + schemaFilePath + "' FORMAT gql";
-
-                Console.WriteLine("Sending query: {0}", schemaQuery);
-
-                var schema_result = client.Query(sec, tx, schemaQuery, "sones.gql");
-
-                Console.WriteLine("... {0}", schema_result.TypeOfResult);
-
-                if (schema_result.TypeOfResult != ResultType.Successful && schema_result.Error != null)
-                    Console.WriteLine(schema_result.Error.Message);
-                    
-                #endregion
-
-                
-
-                string importQuery = null;
-
-                if (isVerbose.Equals("True", StringComparison.InvariantCultureIgnoreCase))
+                finally
                 {
-                    importQuery = "IMPORT FROM 'file:\\\\" + dataFilePath + "' FORMAT fastimport VERBOSITY full";
+                    process.StandardInput.WriteLine("shutdown");
+                    process.Close();
                 }
-                else
-                {
-                    importQuery = "IMPORT FROM 'file:\\\\" + dataFilePath + "' FORMAT fastimport";
-                }
+            }
 
-                Stopwatch sw = new Stopwatch();
-
-                Console.WriteLine("Sending query: {0}", importQuery);
-
-                Console.WriteLine("Start: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString());
-                sw.Start();
-
-                QueryResult rs = client.Query(sec, tx, importQuery, "sones.gql");
-
-                sw.Stop();
-
-                if (rs.Error != null)
-                    Console.WriteLine(rs.Error.Message);
-
-                Console.WriteLine("Stop: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString());
-                Console.WriteLine("Elapsed: " + sw.ElapsedMilliseconds + " ms");
-        }
         
         }
 
