15befbfc77591bfcff691d662fa0193319f31af4
Daniel Kirstenpfad <daniel@sones.de>
2011-09-23 12:19:59 +0200

    * fix for GRAPHDB-504

--- GraphDB/Implementations/SonesGraphDB/Manager/Index/IndexManager.cs
+++ GraphDB/Implementations/SonesGraphDB/Manager/Index/IndexManager.cs
@@ -492,6 +492,20 @@ namespace sones.GraphDB.Manager.Index
 
                 parameter = FillOptions(parameter, options);
 
+                // HACK: manually inject eventuall existing PersistenceLocation (btk, 23.09.2011)
+                #region Hack
+                if (_applicationSettings.Get<PersistenceLocation>() != null)
+                {
+                    // if not already, initialize
+                    if (parameter == null)
+                        parameter = new Dictionary<string, object>();
+
+                    if (!parameter.ContainsKey("Path")) // only add when not already in there...
+                        parameter.Add("Path", _applicationSettings.Get<PersistenceLocation>());
+                }
+                #endregion
+
+
                 var index = _pluginManager.GetAndInitializePlugin<IIndex<IComparable, Int64>>(typeClass, parameter, indexID);
 
                 _indices.Add(indexID, index);
