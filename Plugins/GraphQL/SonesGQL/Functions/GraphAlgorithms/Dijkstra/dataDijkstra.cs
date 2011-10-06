8a310db69f94deac798a2cc2b7a624a283da64ca
Andriy <andriy.kupershmidt@fh-erfurt.de>
2011-10-06 14:44:01 +0200

    * Dijkstra (all types of edges whose target vertex is the same type as the start and end vertices) http://forum.sones.de/phpbb/viewtopic.php?f=3&t=26

--- Plugins/GraphQL/SonesGQL/Functions/GraphAlgorithms/Dijkstra/dataDijkstra.cs
+++ Plugins/GraphQL/SonesGQL/Functions/GraphAlgorithms/Dijkstra/dataDijkstra.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using sones.Library.PropertyHyperGraph;
+using sones.GraphDB.TypeSystem;
 
 namespace sones.Plugins.SonesGQL.Functions.Dijkstra
 {
@@ -10,29 +11,29 @@ namespace sones.Plugins.SonesGQL.Functions.Dijkstra
     public class dataDijkstra
     {
 
-        Dictionary<long, Tuple<IVertex, double, ulong, ISingleEdge, IVertex>> list;
+        Dictionary<long, Tuple<IVertex, double, ulong, Tuple<ISingleEdge,IOutgoingEdgeDefinition>, IVertex>> list;
         public int Count { get { return Count; } }
 
         int count;
 
         public dataDijkstra()
         {
-            list = new Dictionary<long, Tuple<IVertex, double, ulong, ISingleEdge, IVertex>>();
+            list = new Dictionary<long, Tuple<IVertex, double, ulong, Tuple<ISingleEdge, IOutgoingEdgeDefinition>, IVertex>>();
             count = 0;
 
         }
-        public void add(IVertex current_node, double current_distance, UInt64 current_depth, ISingleEdge current_edge, IVertex father)
+        public void add(IVertex current_node, double current_distance, UInt64 current_depth, ISingleEdge current_edge,IOutgoingEdgeDefinition edgeType, IVertex father)
         {
             var id = current_node.VertexID;
-            list.Add(id, Tuple.Create(current_node, current_distance, current_depth, current_edge, father));
+            list.Add(id, Tuple.Create(current_node, current_distance, current_depth, Tuple.Create(current_edge,edgeType), father));
             count++;
         }
 
 
-        public void set(IVertex value, double current_distance, ulong current_depth, ISingleEdge current_edge, IVertex father)
+        public void set(IVertex value, double current_distance, ulong current_depth, ISingleEdge current_edge,IOutgoingEdgeDefinition edgeType, IVertex father)
         {
             var key = value.VertexID;
-            list[key] = Tuple.Create(value, current_distance, current_depth, current_edge, father);
+            list[key] = Tuple.Create(value, current_distance, current_depth, Tuple.Create(current_edge,edgeType), father);
 
         }
 
@@ -60,19 +61,19 @@ namespace sones.Plugins.SonesGQL.Functions.Dijkstra
             return list.ElementAt(current_vertexID).Value.Item2;
         }
 
-        public Tuple<IVertex, double, ulong, ISingleEdge, IVertex> getElement(long key)
+        public Tuple<IVertex, double, ulong, Tuple<ISingleEdge,IOutgoingEdgeDefinition>, IVertex> getElement(long key)
         {
-            Tuple<IVertex, double, ulong, ISingleEdge, IVertex> temp;
+            Tuple<IVertex, double, ulong, Tuple<ISingleEdge,IOutgoingEdgeDefinition>, IVertex> temp;
             list.TryGetValue(key, out temp);
             return temp;
         }
 
-        private Tuple<IVertex, double, ulong, ISingleEdge, IVertex> getTuple(int key)
+        private Tuple<IVertex, double, ulong, Tuple<ISingleEdge, IOutgoingEdgeDefinition>, IVertex> getTuple(int key)
         {
             return this.list.ElementAt(key).Value;
         }
 
-        private Tuple<IVertex, double, ulong, ISingleEdge, IVertex> getTuple(long key)
+        private Tuple<IVertex, double, ulong, Tuple<ISingleEdge, IOutgoingEdgeDefinition>, IVertex> getTuple(long key)
         {
             return this.list[key];
         }
